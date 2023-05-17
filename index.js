32347c1ed9eca166c8d96ba9be56651ee2647b8c
Simun Biswas <simunbiswas14395@gmail.com>
2023-05-18 00:53:59 +0530

    Designs and templates added

--- index.js
+++ index.js
@@ -31,8 +31,56 @@ function convertTimeTo12HrFormat(time) {
 function convertFtoCelciusvalue(tempF){
     let tempC = (tempF - 32) * 5 / 9;
     tempC = Math.floor(tempC);
-    return tempC + "°C";    
+    return tempC ;    
     }
+
+
+
+function chooseIcon(icon){
+    if(icon === "clear"){
+        return "images/sun.png";
+    }
+    else if(icon === "clear-day"){
+        return "images/sun.png";
+    }
+    else if(icon === "clear-night"){
+        return "images/moon.png";
+    }
+    else if(icon === "rain"){
+        return "images/rain.png";
+    }
+    else if(icon === "snow"){
+        return "images/snowflake.png";
+    }
+    else if(icon === "fog"){
+        return "images/fog.png";
+    }
+    else if(icon === "wind"){
+        return "images/storm.png";
+    }
+    else if(icon === "cloudy"){
+        return "images/clouds.png";
+    }
+    else if(icon === "partly-cloudy-night"){
+        return "images/night.png";
+    }
+    else if(icon === "partly-cloudy-day"){
+        return "images/partly-cloudy.png";
+    }
+}
+
+function getDayOfWeek(dateString) {
+    var dateParts = dateString.split("-");
+    var year = parseInt(dateParts[0]);
+    var month = parseInt(dateParts[1]) - 1; // months are zero-based
+    var day = parseInt(dateParts[2]);
+  
+    var date = new Date(year, month, day);
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayOfWeek = daysOfWeek[date.getDay()];
+  
+    return dayOfWeek;
+  }
 
 const apiKey = "67J5QTB6W6V53QM7BGURA5ZGM"
 const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" ;
@@ -44,30 +92,62 @@ async function getWeather(city) {
     const response =await fetch(url + city + `?&key=${apiKey}`)
     const data = await response.json();
     console.log(data);
-    console.log(data.address)
     document.getElementById("initial-place").innerHTML = data.address;
     const {latitude, longitude} = data;
  
     marker.setLatLng([latitude, longitude]);
     map.setView([latitude, longitude], 10); 
    
-    const {temp, tempmin, tempmax, humidity,sunset, sunrise, windspeed } = data.days[0];
-    document.getElementById("location").innerHTML = data.address;
+    const {feelslike, temp, tempmin, tempmax,datetime, humidity,sunset, sunrise, windspeed,icon, description } = data.days[0];
+    document.getElementById("initial-place").innerHTML = data.address;
+    console.log(icon);
+    console.log(description);
 
     const tempC = convertFtoCelciusvalue(temp);
     const max_tempC = convertFtoCelciusvalue(tempmax);
     const min_tempC = convertFtoCelciusvalue(tempmin);
+    const feels_like = convertFtoCelciusvalue(feelslike);
     const sunriseT =convertTimeTo12HrFormat(sunrise);
     const sunsetT = convertTimeTo12HrFormat(sunset);
 
-    document.getElementById("temp").innerHTML = tempC;
-    document.getElementById("max-temp").innerHTML = max_tempC;
-    document.getElementById("min-temp").innerHTML = min_tempC;
-    document.getElementById("humidity").innerHTML = humidity + "%";
-    document.getElementById("wind").innerHTML = windspeed;
+    document.getElementById("temp").innerHTML = tempC + "°C";
+    document.getElementById("max-temp").innerHTML = max_tempC + "°C";
+    document.getElementById("min-temp").innerHTML = min_tempC + "°C";
+    document.getElementById("feels-like").innerHTML = " " + feels_like;
+    document.getElementById("description").innerHTML = description ;
+    document.getElementById("humidity").innerHTML = humidity;
+    document.getElementById("wind").innerHTML = windspeed ;
     document.getElementById("sunrise").innerHTML = sunriseT;
     document.getElementById("sunset").innerHTML = sunsetT;
+    // document.getElementById("input-value").reset();
+
+    const source = chooseIcon(icon);   
+    const mainIcon = document.getElementById("main-icon");
+    mainIcon.src = source 
     
+    document.getElementById("day-1").innerHTML = getDayOfWeek(data.days[1].datetime);
+    document.getElementById("day-2").innerHTML = getDayOfWeek(data.days[2].datetime);
+    document.getElementById("day-3").innerHTML = getDayOfWeek(data.days[3].datetime);
+    document.getElementById("day-4").innerHTML = getDayOfWeek(data.days[4].datetime);
+
+    document.getElementById("main-icon-0").innerHTML.src = chooseIcon(data.days[0].icon);     
+    document.getElementById("main-icon-1").innerHTML.src = chooseIcon(data.days[1].icon);     
+    document.getElementById("main-icon-2").innerHTML.src = chooseIcon(data.days[2].icon);     
+    document.getElementById("main-icon-3").innerHTML.src = chooseIcon(data.days[3].icon);     
+    document.getElementById("main-icon-4").innerHTML.src = chooseIcon(data.days[4].icon);  
+    
+    document.getElementById("max-temp-1").innerHTML = convertFtoCelciusvalue(data.days[1].tempmax);
+    document.getElementById("max-temp-01").innerHTML = convertFtoCelciusvalue(data.days[1].tempmax);
+    document.getElementById("max-temp-2").innerHTML = convertFtoCelciusvalue(data.days[2].tempmax);
+    document.getElementById("max-temp-3").innerHTML = convertFtoCelciusvalue(data.days[3].tempmax);
+    document.getElementById("max-temp-4").innerHTML = convertFtoCelciusvalue(data.days[4].tempmax);
+
+    document.getElementById("min-temp-1").innerHTML = convertFtoCelciusvalue(data.days[1].tempmin);
+    document.getElementById("min-temp-01").innerHTML = convertFtoCelciusvalue(data.days[1].tempmin);
+    document.getElementById("min-temp-2").innerHTML = convertFtoCelciusvalue(data.days[2].tempmin);
+    document.getElementById("min-temp-3").innerHTML = convertFtoCelciusvalue(data.days[3].tempmin);
+    document.getElementById("min-temp-4").innerHTML = convertFtoCelciusvalue(data.days[4].tempmin);
+
 
 }
 const submitBtn = document.getElementById('submit-btn');
